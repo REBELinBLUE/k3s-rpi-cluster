b9a51417174ae01317e92ecdf19fe82d9c06a338
Stephen Ball <stephen@rebelinblue.com>
2025-12-09 22:53:49 +0000

    Add kured secrets

--- scripts/get-sealed-secret-cert.sh
+++ scripts/get-sealed-secret-cert.sh
@@ -4,4 +4,12 @@ set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-kubeseal --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert
+source ".env"
+
+kubeseal --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert > pub-sealed-secrets.pem
+
+kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic kured \
+  --from-literal=KURED_SLACK_HOOK_URL=$SLACK_URL \
+  --dry-run=client -o yaml | \
+kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/kured/secrets.yaml
+
