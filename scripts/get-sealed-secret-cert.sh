ac6004a006bd05fbef1fd689d6e48f40cb72ef7f
Stephen Ball <stephen@rebelinblue.com>
2025-12-09 22:39:27 +0000

    test

--- scripts/get-sealed-secret-cert.sh
+++ scripts/get-sealed-secret-cert.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
-kubectl --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+kubeseal --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert
