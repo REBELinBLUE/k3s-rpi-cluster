f1c5196f41043ba1a0952ae043b9f634adf3e726
Stephen Ball <stephen@rebelinblue.com>
2025-12-09 23:01:32 +0000

    Add traefik forward auth

--- scripts/get-sealed-secret-cert.sh
+++ scripts/get-sealed-secret-cert.sh
@@ -14,3 +14,11 @@ kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic kured \
   --dry-run=client -o yaml | \
 kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/kured/secrets.yaml
 
+
+kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic traefik-forward-auth \
+  --namespace=ingress \
+  --from-literal=CLIENT_ID=$OAUTH_CLIENT_ID \
+  --from-literal=CLIENT_SECRET=$OAUTH_CLIENT_SECRET \
+  --from-literal=SECRET=$OAUTH_SECRET \
+  --dry-run=client -o yaml | \
+kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/traefik-forward-auth/secrets.yaml
