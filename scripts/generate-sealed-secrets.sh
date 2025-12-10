ba80458e3cb3eeb3a95eb0d30e824a496b779c96
Stephen Ball <stephen@rebelinblue.com>
2025-12-10 00:16:13 +0000

    Update sealed secrets

--- scripts/generate-sealed-secrets.sh
+++ scripts/generate-sealed-secrets.sh
@@ -4,21 +4,4 @@ set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-source ".env"
-
-kubeseal --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert > pub-sealed-secrets.pem
-
-kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic kured \
-  --namespace=kube-system \
-  --from-literal=NOTIFY_URL=$SLACK_URL \
-  --dry-run=client -o yaml | \
-kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/kured/secrets.yaml
-
-
-kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic traefik-forward-auth \
-  --namespace=ingress \
-  --from-literal=CLIENT_ID=$OAUTH_CLIENT_ID \
-  --from-literal=CLIENT_SECRET=$OAUTH_CLIENT_SECRET \
-  --from-literal=SECRET=$OAUTH_SECRET \
-  --dry-run=client -o yaml | \
-kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/traefik-forward-auth/secrets.yaml
+op run --account my.1password.com --env-file="$REPO_ROOT/.env" -- ./scripts/populate-secrets-manifests.sh
---
+++ scripts/populate-secrets-manifests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -euo pipefail
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+kubeseal --kubeconfig="$REPO_ROOT/k3s_config" --controller-namespace kube-system --controller-name sealed-secrets-controller --fetch-cert > pub-sealed-secrets.pem
+
+kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic kured \
+  --namespace=kube-system \
+  --from-literal=NOTIFY_URL="$SLACK_URL" \
+  --dry-run=client -o yaml | \
+  kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/kured/secrets.yaml
+
+kubectl --kubeconfig="$REPO_ROOT/k3s_config" create secret generic traefik-forward-auth \
+  --namespace=ingress \
+  --from-literal=CLIENT_ID="$OAUTH_CLIENT_ID" \
+  --from-literal=CLIENT_SECRET="$OAUTH_CLIENT_SECRET" \
+  --from-literal=SECRET="$OAUTH_SECRET" \
+  --dry-run=client -o yaml | \
+  kubeseal --kubeconfig="$REPO_ROOT/k3s_config" -o yaml > $REPO_ROOT/manifests/infrastructure/manifests/traefik-forward-auth/secrets.yaml
