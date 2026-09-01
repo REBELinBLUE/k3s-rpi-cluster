c6556f128441176d9261468c34e2ab30a9235b2b
Stephen Ball <stephen@rebelinblue.com>
2026-09-01 12:20:56 +0100

    remove kured

--- scripts/get-dashboard-token.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -euo pipefail
-
-REPO_ROOT=$(git rev-parse --show-toplevel)
-
-kubectl --kubeconfig="$REPO_ROOT/k3s_config" -n kubernetes-dashboard get secret admin-user -o jsonpath={".data.token"} | base64 -d
