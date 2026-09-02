25aa4a172b41a10d51d45f53d960f9e17418f7f8
Stephen Ball <stephen@rebelinblue.com>
2026-09-02 15:52:23 +0100

    Add script

---
+++ scripts/get-grafana-password.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -euo pipefail
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+PWD=$(kubectl --kubeconfig="$REPO_ROOT/k3s_config" -n monitoring get secrets vm-grafana -o jsonpath="{.data.admin-password}" | base64 -d)
+
+echo "Grafana initial admin password: $PWD"
+echo ""
+echo "Forwarding Grafana server to http://localhost:8080"
+
+kubectl --kubeconfig="$REPO_ROOT/k3s_config" -n monitoring port-forward service/vm-grafana 8080:80
