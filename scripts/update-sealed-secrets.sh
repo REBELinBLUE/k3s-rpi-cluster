04c0e687f1d1ccbed8bc2c0853b45763a4fb788b
Stephen Ball <stephen@rebelinblue.com>
2026-09-06 00:42:02 +0100

    Change to install kube config locally in the proper place

--- scripts/update-sealed-secrets.sh
+++ scripts/update-sealed-secrets.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
-
 op run --environment jj5r3i2pf4cnxj6hak7jzhgtyy --account my.1password.com -- ./scripts/populate-secrets-manifests.sh
 
 git add manifests/**/sealed-secrets.yaml
