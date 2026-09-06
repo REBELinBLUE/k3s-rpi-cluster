5270c531433d7d6a1e6c7df04899abc2f931139e
Stephen Ball <stephen@rebelinblue.com>
2026-09-06 22:38:27 +0100

    Fix path in update-sealed-secrets

--- scripts/update-sealed-secrets.sh
+++ scripts/update-sealed-secrets.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 op run --environment jj5r3i2pf4cnxj6hak7jzhgtyy --account my.1password.com -- ./scripts/populate-secrets-manifests.sh
 
-git add manifests/**/sealed-secrets.yaml
+git add gitops/**/sealed-secrets.yaml
 git commit -m "Update sealed secrets" || echo "No changes to commit"
 git push
